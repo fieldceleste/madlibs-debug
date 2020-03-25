4e77b655ee41f4ca89c6a10ebb5ed28d25ebdd4e
apple <apple@Celestes-IMac.local>
2020-03-25 12:42:05 -0700

    add debugging tools to fix page

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  $("#blanks form").submit(function() {
-    alert('Got to beginning of form submit!');
+  $("#blanks form").submit(function(event) {
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
     blanks.forEach(function(blank) {
-      var userInput = $("input." + blank).val();
+      var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput).val();      
     });
 
     $("#story").show();
-    alert('Got to end of form submit!');
+
+    event.preventDefault();
   });
 });
